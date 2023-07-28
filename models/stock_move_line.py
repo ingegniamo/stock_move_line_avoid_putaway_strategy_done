13f9407a92782c3a767e5c514d913703295dd8f1
Michele <dicroce.m@stesi.eu>
2023-07-28 08:46:51 +0200

    metodo migliore

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -6,9 +6,9 @@ from odoo import _, api, Command, fields, models
 
 
 
-class StockMove(models.Model):
+class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     def _apply_putaway_strategy(self):
         self = self.filtered(lambda l: l.qty_done == 0 or not l.picking_id)
-        return super()._apply_putaway_strategy()
+        return super(StockMoveLine,self)._apply_putaway_strategy()
